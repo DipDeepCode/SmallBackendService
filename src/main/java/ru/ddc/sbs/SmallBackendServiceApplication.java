07b9ac12380174a9a71943cfbb64a686ef254261
Sergey Kovalev <dip.deep@mail.ru>
2022-12-02 15:00:23 +0300

    Init commit

---
+++ src/main/java/ru/ddc/sbs/SmallBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.ddc.sbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallBackendServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallBackendServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ddc/sbs/SmallBackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ddc.sbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallBackendServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
